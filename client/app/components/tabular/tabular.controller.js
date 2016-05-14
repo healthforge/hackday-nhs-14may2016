ba07396946b55f4d6cd36546cbcc31c70c5cfce8
Finn Catling <finn.catling@gmail.com>
2016-05-14 16:05:57 +0100

    basic table with dummy data

--- client/app/components/tabular/tabular.controller.js
+++ client/app/components/tabular/tabular.controller.js
@@ -1,25 +1,31 @@
 class TabularController {
-  constructor(LabResultsService, $scope, $timeout) {
+  constructor(LabResultsService, $scope, $timeout, NgTableParams) {
     this.name = 'tabular';
     this.LabResultsService = LabResultsService;
     this.$scope = $scope;
     this.$timeout = $timeout;
-    this.loadData();
+    this.loadData(NgTableParams);
   }
   
-  loadData() {
-    this.data = { plt: [] };
-    var promise = this.LabResultsService.getPlt();
-    var vm = this;
-    promise.then(function(res) {
-      vm.$timeout(function() {
-        vm.data.plt = res;
-      });
+  loadData(NgTableParams) {
+    this.data = [{name: 'Moroni', age: 50} /*,*/];
+    // this.tableParams = new NgTableParams({}, { dataset: this.data})
+    this.tableParams = new NgTableParams(
+      {page: 1, count: 10},
+      {
+        total: 0,
+        data: this.data,
+        getData: function ($defer, params) {
+          var filter = params.filter();
+          var sorting = params.sorting();
+          var count = params.count();
+          var page = params.page();
+          $defer.resolve(this.data);
+      }
     });
-    this.data.ne = this.LabResultsService.getNe();
   }
 }
 
-TabularController.$inject = ['LabResultsService', '$scope', '$timeout'];
+TabularController.$inject = ['LabResultsService', '$scope', '$timeout', 'NgTableParams'];
 
 export default TabularController;
--- client/app/components/tabular/tabular.js
+++ client/app/components/tabular/tabular.js
@@ -1,11 +1,13 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import angularTable from 'ng-table/dist/ng-table.min.js';
 import tabularComponent from './tabular.component';
 import tabularD3 from './tabularD3/tabularD3';
 
 let tabularModule = angular.module('tabular', [
     uiRouter,
-    tabularD3.name
+    tabularD3.name,
+    'ngTable'
 ])
 
     .config(($stateProvider) => {
